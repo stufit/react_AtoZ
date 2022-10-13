36e845edd399a118344c03de090106a78b0d0796
이관영 <stufit@marketboro.com>
2022-10-13 10:40:53 +0900

    Feat: 리액트 todo 초기설치

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,15 @@
+import React , {Component}from 'react';
+import './App.css'
+export default class App extends Component {
+  render() {
+        return (
+            <div className={'container'}>
+                <div className={'todoBlock'}>
+                    <div className={'title'}>
+                        <h1>할일 목록</h1>
+                    </div>
+                </div>
+            </div>
+        )
+}
+              }
