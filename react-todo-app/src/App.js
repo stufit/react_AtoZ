c586b66e430ef52d45477d32f8c2374599c98f51
이관영 <stufit@marketboro.com>
2022-10-13 11:56:16 +0900

    Feat: 할일목록 UI 만들기

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -2,12 +2,34 @@ import React, {Component} from 'react';
 import "./App.css";
 
 export default class App extends Component {
+	btnStyle ={
+		color: "#fff",
+		border: "none",
+		padding: "5px 8px",
+		borderRadius: "50%",
+		cursor: "pointer",
+    float: "right"
+	}
+
+	// 체크하면 선을 그어야 하기 때문에 동적으로 해야되서 함수로 만듬.
+	getStyle = () =>{
+		return {
+			padding: "10px",
+			borderBottom: "1px #ccc dotted",
+			textDecoration: "none",
+		}
+	}
   render() {
     return (
 	    <div className="container">
 		    <div className="todoBlock">
 			    <div className="title">
 				    <h1>할 일 목록</h1>
+			    </div>
+			    <div style={this.getStyle()}>
+				    <input type="checkbox" defaultChecked={false} />
+				    공부하기
+				    <button style={this.btnStyle}>x</button>
 			    </div>
 		    </div>
 	    </div>
